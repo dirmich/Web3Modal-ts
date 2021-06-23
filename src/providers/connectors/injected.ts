e23b980b73b8a29c277494de56f032878a0bb10f
David, Shin <dirmich@gmail.com>
2021-06-23 10:58:53 +0900

    first commit

---
+++ src/providers/connectors/injected.ts
@@ -0,0 +1,24 @@
+const ConnectToInjected = async () => {
+  let provider = null
+  if ((window as any).ethereum) {
+    provider = (window as any).ethereum
+    try {
+      // await (window as any).ethereum.enable();
+      await (window as any).ethereum.request({ method: 'eth_requestAccounts' })
+    } catch (error) {
+      throw new Error('User Rejected')
+    }
+  } else if ((window as any).web3) {
+    provider = (window as any).web3.currentProvider
+    try {
+      await provider.enable()
+    } catch (error) {
+      throw new Error('User Rejected')
+    }
+  } else {
+    throw new Error('No Web3 Provider found')
+  }
+  return provider
+}
+
+export default ConnectToInjected
